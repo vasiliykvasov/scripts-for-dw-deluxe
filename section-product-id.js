8f075185bb8ca572c7114245d247d83443246ada
Vasiliy Kvasov <vasiliy.s.kireev@gmail.com>
2023-05-19 18:33:48 +0300

    Update section-product-id.js

--- section-product-id.js
+++ section-product-id.js
@@ -1,4 +1,4 @@
-/* Скрипт для получения всех идентификаторов товаров в разделе в шаблоне DW Deluxe */
+/* Скрипт для получения идентификаторов товаров в разделе в шаблоне DW Deluxe */
 let productSkuList = document.querySelectorAll("#catalogSection .productList .sku");
 console.log(document.querySelector("h1").innerText + ": " + productSkuList.length);
 console.log("{");
@@ -6,3 +6,12 @@ for (let productSku of productSkuList) {
 console.log(productSku.dataset.productId);
 }
 console.log("}");
+
+/* Скрипт для получения всех идентификаторов товаров в одном сообщении в разделе в шаблоне DW Deluxe */
+let productSkuList = document.querySelectorAll("#catalogSection .productList .sku");
+let productsId = new Array;
+console.log(document.querySelector("h1").innerText + ": " + productSkuList.length);
+for (let productSku of productSkuList) {
+    productsId = productsId + productSku.dataset.productId + "\n";
+}
+console.log(productsId);
