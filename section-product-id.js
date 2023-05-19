ae2be52de0009ac9f46f00b48c872458ce65aa6a
Vasiliy Kvasov <vasiliy.s.kireev@gmail.com>
2023-05-19 17:09:27 +0300

    Update section-product-id.js

--- section-product-id.js
+++ section-product-id.js
@@ -1,6 +1,6 @@
 /* Скрипт для получения всех идентификаторов товаров в разделе в шаблоне DW Deluxe */
-let productSkuList = document.querySelectorAll(".productList .sku");
-console.log(document.querySelector("h1").innerText);
+let productSkuList = document.querySelectorAll("#catalogSection .productList .sku");
+console.log(document.querySelector("h1").innerText + " " + productSkuList.length);
 console.log("{");
 for (let productSku of productSkuList) {
 console.log(productSku.dataset.productId);
